960dda228712c2b46318431376ad33f1d37d091c
Brandon Streiff <git@streiff.net>
2014-01-25 14:27:30 -0600

    If using SSE2, we shouldn't mess with controlfp.

--- win32/sys_win.c
+++ win32/sys_win.c
@@ -653,8 +653,6 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 		} while (time < 1);
 //			Con_Printf ("time:%5.2f - %5.2f = %5.2f\n", newtime, oldtime, time);
 
-		//	_controlfp( ~( _EM_ZERODIVIDE /*| _EM_INVALID*/ ), _MCW_EM );
-		_controlfp( _PC_24, _MCW_PC );
 		Qcommon_Frame (time);
 
 		oldtime = newtime;
