dbbbadc0facc8ae4880204071b771a9faec86423
Brandon Streiff <git@streiff.net>
2014-02-10 21:14:33 -0600

    Rename snd_win.c -> snd_sdl.c.
    No more Windowsisms in that file!

--- client/snd_sdl.c
+++ client/snd_sdl.c
@@ -17,8 +17,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-#include "../client/client.h"
-#include "../client/snd_loc.h"
+#include "client.h"
+#include "snd_loc.h"
 #include "SDL.h"
 
 static qboolean snd_init;
