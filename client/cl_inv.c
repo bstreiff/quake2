bstreiff/quake2

commit dfefda6b7b9b283f498a36b555ec419ed7fd194c

Add vid_hudscale.
vid_hudscale scales the HUD, menus, and console by a given multiplier (for example, 2.0 will double the size of everything). Useful for high-resolution displays.

Note that those images do not get resampled with gl_texscale, so while fractional values will work, they are likely to look terrible.

client/cl_scrn.c
@@ -58,6 +58,8 @@ cvar_t		*scr_graphscale;
 cvar_t		*scr_graphshift;
 cvar_t		*scr_drawall;
 
+extern cvar_t      *vid_hudscale;
+
 typedef struct
 {
 	int		x1, y1, x2, y2;
@@ -272,17 +274,17 @@ void SCR_DrawCenterString (void)
 		for (l=0 ; l<40 ; l++)
 			if (start[l] == '\n' || !start[l])
 				break;
-		x = (viddef.width - l*8)/2;
+      x = (viddef.width - l * 8 * vid_hudscale->value) / 2;
 		SCR_AddDirtyPoint (x, y);
-		for (j=0 ; j<l ; j++, x+=8)
+      for (j = 0; j<l; j++, x += 8 * vid_hudscale->value)
 		{
 			re.DrawChar (x, y, start[j]);	
 			if (!remaining--)
 				return;
 		}
-		SCR_AddDirtyPoint (x, y+8);
+      SCR_AddDirtyPoint(x, y + 8 * vid_hudscale->value);
 			
-		y += 8;
+      y += 8 * vid_hudscale->value;
 
 		while (*start && *start != '\n')
 			start++;
@@ -424,7 +426,7 @@ void SCR_DrawPause (void)
 		return;
 
 	re.DrawGetPicSize (&w, &h, "pause");
-	re.DrawPic ((viddef.width-w)/2, viddef.height/2 + 8, "pause");
+   re.DrawPic((viddef.width - w) / 2, viddef.height / 2 + 8 * vid_hudscale->value, "pause");
 }
 
 /*
@@ -711,8 +713,8 @@ void SizeHUDString (char *string, int *w, int *h)
 		string++;
 	}
 
-	*w = width * 8;
-	*h = lines * 8;
+   *w = width * 8 * vid_hudscale->value;
+   *h = lines * 8 * vid_hudscale->value;
 }
 
 void DrawHUDString (char *string, int x, int y, int centerwidth, int xor)
@@ -733,19 +735,19 @@ void DrawHUDString (char *string, int x, int y, int centerwidth, int xor)
 		line[width] = 0;
 
 		if (centerwidth)
-			x = margin + (centerwidth - width*8)/2;
+         x = margin + (centerwidth - width * 8 * vid_hudscale->value) / 2;
 		else
 			x = margin;
 		for (i=0 ; i<width ; i++)
 		{
 			re.DrawChar (x, y, line[i]^xor);
-			x += 8;
+         x += 8 * vid_hudscale->value;
 		}
 		if (*string)
 		{
 			string++;	// skip the \n
 			x = margin;
-			y += 8;
+         y += 8 * vid_hudscale->value;
 		}
 	}
 }
@@ -769,14 +771,14 @@ void SCR_DrawField (int x, int y, int color, int width, int value)
 	if (width > 5)
 		width = 5;
 
-	SCR_AddDirtyPoint (x, y);
-	SCR_AddDirtyPoint (x+width*CHAR_WIDTH+2, y+23);
+	SCR_AddDirtyPoint(x, y);
+   SCR_AddDirtyPoint(x + width*CHAR_WIDTH*vid_hudscale->value + 2, y + 23 * vid_hudscale->value);
 
 	Com_sprintf (num, sizeof(num), "%i", value);
 	l = strlen(num);
 	if (l > width)
 		l = width;
-	x += 2 + CHAR_WIDTH*(width - l);
+   x += 2 + CHAR_WIDTH*(width - l) * vid_hudscale->value;
 
 	ptr = num;
 	while (*ptr && l)
@@ -787,7 +789,7 @@ void SCR_DrawField (int x, int y, int color, int width, int value)
 			frame = *ptr -'0';
 
 		re.DrawPic (x,y,sb_nums[color][frame]);
-		x += CHAR_WIDTH;
+      x += CHAR_WIDTH * vid_hudscale->value;
 		ptr++;
 		l--;
 	}
@@ -835,6 +837,8 @@ void SCR_ExecuteLayoutString (char *s)
 	int		width;
 	int		index;
 	clientinfo_t	*ci;
+   const int virtual_width = 320 * vid_hudscale->value;
+   const int virtual_height = 240 * vid_hudscale->value;
 
 	if (cls.state != ca_active || !cl.refresh_prepped)
 		return;
@@ -852,38 +856,38 @@ void SCR_ExecuteLayoutString (char *s)
 		if (!strcmp(token, "xl"))
 		{
 			token = COM_Parse (&s);
-			x = atoi(token);
+         x = atoi(token) * vid_hudscale->value;
 			continue;
 		}
 		if (!strcmp(token, "xr"))
 		{
 			token = COM_Parse (&s);
-			x = viddef.width + atoi(token);
+         x = viddef.width + atoi(token) * vid_hudscale->value;
 			continue;
 		}
 		if (!strcmp(token, "xv"))
 		{
 			token = COM_Parse (&s);
-			x = viddef.width/2 - 160 + atoi(token);
+         x = viddef.width / 2 - virtual_width / 2 + atoi(token) * vid_hudscale->value;
 			continue;
 		}
 
 		if (!strcmp(token, "yt"))
 		{
 			token = COM_Parse (&s);
-			y = atoi(token);
+         y = atoi(token) * vid_hudscale->value;
 			continue;
 		}
 		if (!strcmp(token, "yb"))
 		{
 			token = COM_Parse (&s);
-			y = viddef.height + atoi(token);
+         y = viddef.height + atoi(token) * vid_hudscale->value;
 			continue;
 		}
 		if (!strcmp(token, "yv"))
 		{
 			token = COM_Parse (&s);
-			y = viddef.height/2 - 120 + atoi(token);
+         y = viddef.height / 2 - virtual_height / 2 + atoi(token) * vid_hudscale->value;
 			continue;
 		}
 
@@ -907,9 +911,9 @@ void SCR_ExecuteLayoutString (char *s)
 			int		score, ping, time;
 
 			token = COM_Parse (&s);
-			x = viddef.width/2 - 160 + atoi(token);
+         x = viddef.width / 2 - virtual_width / 2 + atoi(token);
 			token = COM_Parse (&s);
-			y = viddef.height/2 - 120 + atoi(token);
+         y = viddef.height / 2 - virtual_height / 2 + atoi(token);
 			SCR_AddDirtyPoint (x, y);
 			SCR_AddDirtyPoint (x+159, y+31);
 
@@ -946,9 +950,9 @@ void SCR_ExecuteLayoutString (char *s)
 			char	block[80];
 
 			token = COM_Parse (&s);
-			x = viddef.width/2 - 160 + atoi(token);
+         x = viddef.width / 2 - virtual_width / 2 + atoi(token);
 			token = COM_Parse (&s);
-			y = viddef.height/2 - 120 + atoi(token);
+         y = viddef.height / 2 - virtual_height / 2 + atoi(token);
 			SCR_AddDirtyPoint (x, y);
 			SCR_AddDirtyPoint (x+159, y+31);
 
@@ -1195,6 +1199,10 @@ void SCR_UpdateScreen (void)
 		separation[1] = 0;
 		numframes = 1;
 	}
+
+   // Range-check hudscale to avoid division-by-zero.
+   if (vid_hudscale->value < 0.1)
+      Cvar_SetValue("vid_hudscale", 0.1);
 
 	for ( i = 0; i < numframes; i++ )
 	{

commit 5084aa5111d7cf2487c046af3f4bdc91d7676995

Fix spacing issues introduced by dfefda6 and b33dd62
My editor was replacing with spaces in a non-uniform fashion.

client/cl_inv.c
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "client.h"
 
-extern cvar_t*    vid_hudscale;
+extern cvar_t*	vid_hudscale;
 
 /*
 ================
@@ -44,7 +44,7 @@ Inv_DrawString
 */
 void Inv_DrawString (int x, int y, char *string)
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	while (*string)
 	{
 		re.DrawChar (x, y, *string);
@@ -68,7 +68,7 @@ CL_DrawInventory
 
 void CL_DrawInventory (void)
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int		i, j;
 	int		num, selected_num, item;
 	int		index[MAX_ITEMS];

client/cl_scrn.c
@@ -57,7 +57,7 @@ cvar_t		*scr_graphheight;
 cvar_t		*scr_graphscale;
 cvar_t		*scr_graphshift;
 
-extern cvar_t      *vid_hudscale;
+extern cvar_t	*vid_hudscale;
 
 char		crosshair_pic[MAX_QPATH];
 int			crosshair_width, crosshair_height;
@@ -266,15 +266,15 @@ void SCR_DrawCenterString (void)
 		for (l=0 ; l<40 ; l++)
 			if (start[l] == '\n' || !start[l])
 				break;
-      x = (viddef.width - l * 8 * vid_hudscale->value) / 2;
-      for (j = 0; j<l; j++, x += 8 * vid_hudscale->value)
+		x = (viddef.width - l * 8 * vid_hudscale->value) / 2;
+		for (j = 0; j<l; j++, x += 8 * vid_hudscale->value)
 		{
 			re.DrawChar (x, y, start[j]);	
 			if (!remaining--)
 				return;
 		}
 			
-      y += 8 * vid_hudscale->value;
+		y += 8 * vid_hudscale->value;
 
 		while (*start && *start != '\n')
 			start++;
@@ -415,7 +415,7 @@ void SCR_DrawPause (void)
 		return;
 
 	re.DrawGetPicSize (&w, &h, "pause");
-   re.DrawPic((viddef.width - w) / 2, viddef.height / 2 + 8 * vid_hudscale->value, "pause");
+	re.DrawPic((viddef.width - w) / 2, viddef.height / 2 + 8 * vid_hudscale->value, "pause");
 }
 
 /*
@@ -659,8 +659,8 @@ void SizeHUDString (char *string, int *w, int *h)
 		string++;
 	}
 
-   *w = width * 8 * vid_hudscale->value;
-   *h = lines * 8 * vid_hudscale->value;
+	*w = width * 8 * vid_hudscale->value;
+	*h = lines * 8 * vid_hudscale->value;
 }
 
 void DrawHUDString (char *string, int x, int y, int centerwidth, int xor)
@@ -681,19 +681,19 @@ void DrawHUDString (char *string, int x, int y, int centerwidth, int xor)
 		line[width] = 0;
 
 		if (centerwidth)
-         x = margin + (centerwidth - width * 8 * vid_hudscale->value) / 2;
+			x = margin + (centerwidth - width * 8 * vid_hudscale->value) / 2;
 		else
 			x = margin;
 		for (i=0 ; i<width ; i++)
 		{
 			re.DrawChar (x, y, line[i]^xor);
-         x += 8 * vid_hudscale->value;
+			x += 8 * vid_hudscale->value;
 		}
 		if (*string)
 		{
 			string++;	// skip the \n
 			x = margin;
-         y += 8 * vid_hudscale->value;
+			y += 8 * vid_hudscale->value;
 		}
 	}
 }
@@ -721,7 +721,7 @@ void SCR_DrawField (int x, int y, int color, int width, int value)
 	l = strlen(num);
 	if (l > width)
 		l = width;
-   x += 2 + CHAR_WIDTH*(width - l) * vid_hudscale->value;
+	x += 2 + CHAR_WIDTH*(width - l) * vid_hudscale->value;
 
 	ptr = num;
 	while (*ptr && l)
@@ -732,7 +732,7 @@ void SCR_DrawField (int x, int y, int color, int width, int value)
 			frame = *ptr -'0';
 
 		re.DrawPic (x,y,sb_nums[color][frame]);
-      x += CHAR_WIDTH * vid_hudscale->value;
+		x += CHAR_WIDTH * vid_hudscale->value;
 		ptr++;
 		l--;
 	}
@@ -780,9 +780,9 @@ void SCR_ExecuteLayoutString (char *s)
 	int		width;
 	int		index;
 	clientinfo_t	*ci;
-   const float scale = vid_hudscale->value;
-   const int virtual_width = 320 * scale;
-   const int virtual_height = 240 * scale;
+	const float scale = vid_hudscale->value;
+	const int virtual_width = 320 * scale;
+	const int virtual_height = 240 * scale;
 
 	if (cls.state != ca_active || !cl.refresh_prepped)
 		return;
@@ -800,38 +800,38 @@ void SCR_ExecuteLayoutString (char *s)
 		if (!strcmp(token, "xl"))
 		{
 			token = COM_Parse (&s);
-         x = atoi(token) * scale;
+			x = atoi(token) * scale;
 			continue;
 		}
 		if (!strcmp(token, "xr"))
 		{
 			token = COM_Parse (&s);
-         x = viddef.width + atoi(token) * scale;
+			x = viddef.width + atoi(token) * scale;
 			continue;
 		}
 		if (!strcmp(token, "xv"))
 		{
 			token = COM_Parse (&s);
-         x = viddef.width / 2 - virtual_width / 2 + atoi(token) * scale;
+			x = viddef.width / 2 - virtual_width / 2 + atoi(token) * scale;
 			continue;
 		}
 
 		if (!strcmp(token, "yt"))
 		{
 			token = COM_Parse (&s);
-         y = atoi(token) * scale;
+			y = atoi(token) * scale;
 			continue;
 		}
 		if (!strcmp(token, "yb"))
 		{
 			token = COM_Parse (&s);
-         y = viddef.height + atoi(token) * scale;
+			y = viddef.height + atoi(token) * scale;
 			continue;
 		}
 		if (!strcmp(token, "yv"))
 		{
 			token = COM_Parse (&s);
-         y = viddef.height / 2 - virtual_height / 2 + atoi(token) * scale;
+			y = viddef.height / 2 - virtual_height / 2 + atoi(token) * scale;
 			continue;
 		}
 
@@ -853,9 +853,9 @@ void SCR_ExecuteLayoutString (char *s)
 			int		score, ping, time;
 
 			token = COM_Parse (&s);
-         x = viddef.width / 2 - virtual_width / 2 + atoi(token)*scale;
+			x = viddef.width / 2 - virtual_width / 2 + atoi(token)*scale;
 			token = COM_Parse (&s);
-         y = viddef.height / 2 - virtual_height / 2 + atoi(token)*scale;
+			y = viddef.height / 2 - virtual_height / 2 + atoi(token)*scale;
 
 			token = COM_Parse (&s);
 			value = atoi(token);
@@ -890,9 +890,9 @@ void SCR_ExecuteLayoutString (char *s)
 			char	block[80];
 
 			token = COM_Parse (&s);
-         x = viddef.width / 2 - virtual_width / 2 + atoi(token)*scale;
+			x = viddef.width / 2 - virtual_width / 2 + atoi(token)*scale;
 			token = COM_Parse (&s);
-         y = viddef.height / 2 - virtual_height / 2 + atoi(token)*scale;
+			y = viddef.height / 2 - virtual_height / 2 + atoi(token)*scale;
 
 			token = COM_Parse (&s);
 			value = atoi(token);
@@ -1136,9 +1136,9 @@ void SCR_UpdateScreen (void)
 		numframes = 1;
 	}
 
-   // Range-check hudscale to avoid division-by-zero.
-   if (vid_hudscale->value < 0.1)
-      Cvar_SetValue("vid_hudscale", 0.1);
+	// Range-check hudscale to avoid division-by-zero.
+	if (vid_hudscale->value < 0.1)
+		Cvar_SetValue("vid_hudscale", 0.1);
 
 	for ( i = 0; i < numframes; i++ )
 	{

client/console.c
@@ -38,7 +38,7 @@ void DrawString (int x, int y, char *s)
 	while (*s)
 	{
 		re.DrawChar (x, y, *s);
-      x += 8 * vid_hudscale->value;
+		x += 8 * vid_hudscale->value;
 		s++;
 	}
 }
@@ -48,7 +48,7 @@ void DrawAltString (int x, int y, char *s)
 	while (*s)
 	{
 		re.DrawChar (x, y, *s ^ 0x80);
-      x += 8 * vid_hudscale->value;
+		x += 8 * vid_hudscale->value;
 		s++;
 	}
 }
@@ -256,10 +256,10 @@ void Con_CheckResize (void)
 	int		i, j, width, oldwidth, oldtotallines, numlines, numchars;
 	char	tbuf[CON_TEXTSIZE];
 
-   if (viddef.width > 0)
-      width = (viddef.width / (8 * vid_hudscale->value)) - 2;
-   else
-      width = 0;
+	if (viddef.width > 0)
+		width = (viddef.width / (8 * vid_hudscale->value)) - 2;
+	else
+		width = 0;
 
 	if (width == con.linewidth)
 		return;
@@ -533,9 +533,9 @@ void Con_DrawNotify (void)
 		text = con.text + (i % con.totallines)*con.linewidth;
 		
 		for (x = 0 ; x < con.linewidth ; x++)
-         re.DrawChar((x + 1) * 8 * vid_hudscale->value, v, text[x]);
+			re.DrawChar((x + 1) * 8 * vid_hudscale->value, v, text[x]);
 
-      v += 8 * vid_hudscale->value;
+		v += 8 * vid_hudscale->value;
 	}
 
 
@@ -543,31 +543,31 @@ void Con_DrawNotify (void)
 	{
 		if (chat_console)
 		{
-         DrawString(8 * vid_hudscale->value, v, "> ");
+			DrawString(8 * vid_hudscale->value, v, "> ");
 			skip = 3;
 		}
 		else if (chat_team)
 		{
-         DrawString(8 * vid_hudscale->value, v, "say_team:");
+			DrawString(8 * vid_hudscale->value, v, "say_team:");
 			skip = 11;
 		}
 		else
 		{
-         DrawString(8 * vid_hudscale->value, v, "say:");
+			DrawString(8 * vid_hudscale->value, v, "say:");
 			skip = 5;
 		}
 
 		s = chat_buffer;
-      if (chat_bufferlen > (viddef.width / (8 * vid_hudscale->value)) - (skip + 1))
-         s += chat_bufferlen - (int)((viddef.width / (8 * vid_hudscale->value)) - (skip + 1));
+		if (chat_bufferlen > (viddef.width / (8 * vid_hudscale->value)) - (skip + 1))
+			s += chat_bufferlen - (int)((viddef.width / (8 * vid_hudscale->value)) - (skip + 1));
 		x = 0;
 		while(s[x])
 		{
-         re.DrawChar((x + skip) * 8 * vid_hudscale->value, v, s[x]);
+			re.DrawChar((x + skip) * 8 * vid_hudscale->value, v, s[x]);
 			x++;
 		}
-      re.DrawChar((x + skip) * 8 * vid_hudscale->value, v, 10 + ((cls.realtime >> 8) & 1));
-      v += 8 * vid_hudscale->value;
+		re.DrawChar((x + skip) * 8 * vid_hudscale->value, v, 10 + ((cls.realtime >> 8) & 1));
+		v += 8 * vid_hudscale->value;
 	}
 	
 }
@@ -601,28 +601,28 @@ void Con_DrawConsole (float frac)
 
 	Com_sprintf (version, sizeof(version), "v%4.2f", VERSION);
 	for (x=0 ; x<5 ; x++)
-      re.DrawChar(viddef.width - (44 * vid_hudscale->value) + x * 8 * vid_hudscale->value, lines - (12 * vid_hudscale->value), 128 + version[x]);
+		re.DrawChar(viddef.width - (44 * vid_hudscale->value) + x * 8 * vid_hudscale->value, lines - (12 * vid_hudscale->value), 128 + version[x]);
 
 // draw the text
 	con.vislines = lines;
 	
-   rows = (lines - 22) / (8 * vid_hudscale->value);		// rows of text to draw
+	rows = (lines - 22) / (8 * vid_hudscale->value);		// rows of text to draw
 
-   y = lines - (30 * vid_hudscale->value);
+	y = lines - (30 * vid_hudscale->value);
 
 // draw from the bottom up
 	if (con.display != con.current)
 	{
 	// draw arrows to show the buffer is backscrolled
 		for (x=0 ; x<con.linewidth ; x+=4)
-         re.DrawChar((x + 1) * 8 * vid_hudscale->value, y, '^');
+			re.DrawChar((x + 1) * 8 * vid_hudscale->value, y, '^');
 	
-      y -= 8 * vid_hudscale->value;
+		y -= 8 * vid_hudscale->value;
 		rows--;
 	}
 	
 	row = con.display;
-   for (i = 0; i<rows; i++, y -= 8 * vid_hudscale->value, row--)
+	for (i = 0; i<rows; i++, y -= 8 * vid_hudscale->value, row--)
 	{
 		if (row < 0)
 			break;
@@ -632,7 +632,7 @@ void Con_DrawConsole (float frac)
 		text = con.text + (row % con.totallines)*con.linewidth;
 
 		for (x=0 ; x<con.linewidth ; x++)
-         re.DrawChar((x + 1) * 8 * vid_hudscale->value, y, text[x]);
+			re.DrawChar((x + 1) * 8 * vid_hudscale->value, y, text[x]);
 	}
 
 //ZOID

client/menu.c
@@ -252,7 +252,7 @@ void M_DrawCharacter (int cx, int cy, int num)
 
 void M_Print (int cx, int cy, char *str)
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	while (*str)
 	{
 		M_DrawCharacter (cx, cy, (*str)+128);
@@ -263,7 +263,7 @@ void M_Print (int cx, int cy, char *str)
 
 void M_PrintWhite (int cx, int cy, char *str)
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	while (*str)
 	{
 		M_DrawCharacter (cx, cy, *str);
@@ -289,7 +289,7 @@ and both above and below y.
 */
 void M_DrawCursor( int x, int y, int f )
 {
-   char	cursorname[80];
+	char	cursorname[80];
 	static qboolean cached;
 
 	if ( !cached )
@@ -311,8 +311,8 @@ void M_DrawCursor( int x, int y, int f )
 
 void M_DrawTextBox (int x, int y, int width, int lines)
 {
-   const float scale = vid_hudscale->value;
-   int		cx, cy;
+	const float scale = vid_hudscale->value;
+	int		cx, cy;
 	int		n;
 
 	// draw left side
@@ -366,7 +366,7 @@ MAIN MENU
 
 void M_Main_Draw (void)
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int i;
 	int w, h;
 	int ystart;
@@ -519,7 +519,7 @@ static void StartNetworkServerFunc( void *unused )
 
 void Multiplayer_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	s_multiplayer_menu.x = viddef.width * 0.50 - 64 * scale;
 	s_multiplayer_menu.nitems = 0;
 
@@ -676,7 +676,7 @@ static void M_FindKeysForCommand (char *command, int *twokeys)
 
 static void KeyCursorDrawFunc( menuframework_s *menu )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	if ( bind_grab )
 		re.DrawChar( menu->x, menu->y + menu->cursor * 9*scale, '=' );
 	else
@@ -685,7 +685,7 @@ static void KeyCursorDrawFunc( menuframework_s *menu )
 
 static void DrawKeyBindingFunc( void *self )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int keys[2];
 	menuaction_s *a = ( menuaction_s * ) self;
 
@@ -731,7 +731,7 @@ static void KeyBindingFunc( void *self )
 
 static void Keys_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int y = 0;
 	int i = 0;
 
@@ -1156,7 +1156,7 @@ static void ConsoleFunc( void *unused )
 
 static void UpdateSoundQualityFunc( void *unused )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	if ( s_options_quality_list.curvalue )
 	{
 		Cvar_SetValue( "s_khz", 22 );
@@ -1214,7 +1214,7 @@ void Options_MenuInit( void )
 		"angle",
 		0
 	};
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 
 	win_noalttab = Cvar_Get( "win_noalttab", "0", CVAR_ARCHIVE );
 
@@ -1751,7 +1751,7 @@ static const char *roguecredits[] =
 
 void M_Credits_MenuDraw( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int i, y;
 
 	/*
@@ -1931,7 +1931,7 @@ static void CreditsFunc( void *unused )
 
 void Game_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	static const char *difficulty_names[] =
 	{
 		"easy",
@@ -2071,7 +2071,7 @@ void LoadGameCallback( void *self )
 
 void LoadGame_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int i;
 
 	s_loadgame_menu.x = viddef.width / 2 - 120*scale;
@@ -2150,7 +2150,7 @@ void SaveGame_MenuDraw( void )
 
 void SaveGame_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int i;
 
 	s_savegame_menu.x = viddef.width / 2 - 120*scale;
@@ -2271,7 +2271,7 @@ void NullCursorDraw( void *self )
 
 void SearchLocalGames( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int		i;
 
 	m_num_servers = 0;
@@ -2297,8 +2297,8 @@ void SearchLocalGamesFunc( void *self )
 
 void JoinServer_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
-   int i;
+	const float scale = vid_hudscale->value;
+	int i;
 
 	s_joinserver_menu.x = viddef.width * 0.50 - 120*scale;
 	s_joinserver_menu.nitems = 0;
@@ -2320,8 +2320,8 @@ void JoinServer_MenuInit( void )
 
 	s_joinserver_server_title.generic.type = MTYPE_SEPARATOR;
 	s_joinserver_server_title.generic.name = "connect to...";
-	s_joinserver_server_title.generic.x    = 80*scale;
-	s_joinserver_server_title.generic.y	   = 30*scale;
+	s_joinserver_server_title.generic.x	= 80*scale;
+	s_joinserver_server_title.generic.y	= 30*scale;
 
 	for ( i = 0; i < MAX_LOCAL_SERVERS; i++ )
 	{
@@ -2503,7 +2503,7 @@ void StartServerActionFunc( void *self )
 
 void StartServer_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	static const char *dm_coop_names[] =
 	{
 		"deathmatch",
@@ -2571,8 +2571,8 @@ void StartServer_MenuInit( void )
 
 	for ( i = 0; i < nummaps; i++ )
 	{
-    char  shortname[MAX_TOKEN_CHARS];
-    char  longname[MAX_TOKEN_CHARS];
+		char  shortname[MAX_TOKEN_CHARS];
+		char  longname[MAX_TOKEN_CHARS];
 		char  scratch[200];
 		int		j, l;
 
@@ -2916,7 +2916,7 @@ setvalue:
 
 void DMOptions_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	static const char *yes_no_names[] =
 	{
 		"no", "yes", 0
@@ -3189,7 +3189,7 @@ static void DownloadCallback( void *self )
 
 void DownloadOptions_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	static const char *yes_no_names[] =
 	{
 		"no", "yes", 0
@@ -3287,7 +3287,7 @@ static menufield_s		s_addressbook_fields[NUM_ADDRESSBOOK_ENTRIES];
 
 void AddressBook_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int i;
 
 	s_addressbook_menu.x = viddef.width / 2 - 142*scale;
@@ -3605,7 +3605,7 @@ static int pmicmpfnc( const void *_a, const void *_b )
 
 qboolean PlayerConfig_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	extern cvar_t *name;
 	extern cvar_t *team;
 	extern cvar_t *skin;
@@ -3771,7 +3771,7 @@ float CalcFov(float fov_x, float w, float h);
 
 void PlayerConfig_MenuDraw( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	refdef_t refdef;
 	char scratch[MAX_QPATH];
 

client/qmenu.c
@@ -56,7 +56,7 @@ void Action_DoEnter( menuaction_s *a )
 
 void Action_Draw( menuaction_s *a )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	if ( a->generic.flags & QMF_LEFT_JUSTIFY )
 	{
 		if ( a->generic.flags & QMF_GRAYED )
@@ -89,7 +89,7 @@ void Field_Draw( menufield_s *f )
 {
 	int i;
 	char tempbuffer[128]="";
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 
 	if ( f->generic.name )
 		Menu_DrawStringR2LDark( f->generic.x + f->generic.parent->x + LCOLUMN_OFFSET, f->generic.y + f->generic.parent->y, f->generic.name );
@@ -349,7 +349,7 @@ void Menu_Draw( menuframework_s *menu )
 {
 	int i;
 	menucommon_s *item;
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 
 	/*
 	** draw contents
@@ -419,7 +419,7 @@ void Menu_Draw( menuframework_s *menu )
 
 void Menu_DrawStatusBar( const char *string )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	if ( string )
 	{
 		int l = strlen( string );
@@ -437,7 +437,7 @@ void Menu_DrawStatusBar( const char *string )
 
 void Menu_DrawString( int x, int y, const char *string )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	unsigned i;
 
 	for ( i = 0; i < strlen( string ); i++ )
@@ -448,7 +448,7 @@ void Menu_DrawString( int x, int y, const char *string )
 
 void Menu_DrawStringDark( int x, int y, const char *string )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	unsigned i;
 
 	for ( i = 0; i < strlen( string ); i++ )
@@ -459,7 +459,7 @@ void Menu_DrawStringDark( int x, int y, const char *string )
 
 void Menu_DrawStringR2L( int x, int y, const char *string )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	unsigned i;
 
 	for ( i = 0; i < strlen( string ); i++ )
@@ -470,7 +470,7 @@ void Menu_DrawStringR2L( int x, int y, const char *string )
 
 void Menu_DrawStringR2LDark( int x, int y, const char *string )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	unsigned i;
 
 	for ( i = 0; i < strlen( string ); i++ )
@@ -574,7 +574,7 @@ void Menulist_DoEnter( menulist_s *l )
 
 void MenuList_Draw( menulist_s *l )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	const char **n;
 	int y = 0;
 
@@ -615,7 +615,7 @@ void Slider_DoSlide( menuslider_s *s, int dir )
 
 void Slider_Draw( menuslider_s *s )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	int	i;
 
 	Menu_DrawStringR2LDark( s->generic.x + s->generic.parent->x + LCOLUMN_OFFSET*scale,
@@ -660,7 +660,7 @@ void SpinControl_DoSlide( menulist_s *s, int dir )
 
 void SpinControl_Draw( menulist_s *s )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 	char buffer[100];
 
 	if ( s->generic.name )

ref_gl/gl_draw.c
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "gl_local.h"
 
 image_t		*draw_chars;
-cvar_t      *vid_hudscale;
+cvar_t		*vid_hudscale;
 
 extern	qboolean	scrap_dirty;
 void Scrap_Upload (void);
@@ -42,7 +42,7 @@ void Draw_InitLocal (void)
 	qglTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	qglTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-   vid_hudscale = ri.Cvar_Get("vid_hudscale", "1", CVAR_ARCHIVE);
+	vid_hudscale = ri.Cvar_Get("vid_hudscale", "1", CVAR_ARCHIVE);
 }
 
 
@@ -60,7 +60,7 @@ void Draw_Char (int x, int y, int num)
 {
 	int				row, col;
 	float			frow, fcol, size;
-   float       pixel_size;
+	float			pixel_size;
 
 	num &= 255;
 	
@@ -77,7 +77,7 @@ void Draw_Char (int x, int y, int num)
 	fcol = col*0.0625;
 	size = 0.0625;
 
-   pixel_size = 8 * vid_hudscale->value;
+	pixel_size = 8 * vid_hudscale->value;
 
 	GL_Bind (draw_chars->texnum);
 
@@ -85,11 +85,11 @@ void Draw_Char (int x, int y, int num)
 	qglTexCoord2f (fcol, frow);
 	qglVertex2f(x, y);
 	qglTexCoord2f (fcol + size, frow);
-   qglVertex2f(x + pixel_size, y);
+	qglVertex2f(x + pixel_size, y);
 	qglTexCoord2f (fcol + size, frow + size);
-   qglVertex2f(x + pixel_size, y + pixel_size);
+	qglVertex2f(x + pixel_size, y + pixel_size);
 	qglTexCoord2f (fcol, frow + size);
-   qglVertex2f(x, y + pixel_size);
+	qglVertex2f(x, y + pixel_size);
 	qglEnd ();
 }
 
@@ -129,8 +129,8 @@ void Draw_GetPicSize (int *w, int *h, char *pic)
 		*w = *h = -1;
 		return;
 	}
-   *w = gl->width * vid_hudscale->value;
-   *h = gl->height * vid_hudscale->value;
+	*w = gl->width * vid_hudscale->value;
+	*h = gl->height * vid_hudscale->value;
 }
 
 /*
@@ -174,7 +174,7 @@ Draw_Pic
 void Draw_Pic (int x, int y, char *pic)
 {
 	image_t *gl;
-   float    scaled_width, scaled_height;
+	float	scaled_width, scaled_height;
 
 	gl = Draw_FindPic (pic);
 	if (!gl)
@@ -185,19 +185,19 @@ void Draw_Pic (int x, int y, char *pic)
 	if (scrap_dirty)
 		Scrap_Upload ();
 
-   scaled_width = gl->width * vid_hudscale->value;
-   scaled_height = gl->height * vid_hudscale->value;
-   
-   GL_Bind (gl->texnum);
+	scaled_width = gl->width * vid_hudscale->value;
+	scaled_height = gl->height * vid_hudscale->value;
+
+	GL_Bind (gl->texnum);
 	qglBegin (GL_QUADS);
 	qglTexCoord2f (gl->sl, gl->tl);
 	qglVertex2f(x, y);
 	qglTexCoord2f (gl->sh, gl->tl);
-   qglVertex2f(x + scaled_width, y);
+	qglVertex2f(x + scaled_width, y);
 	qglTexCoord2f (gl->sh, gl->th);
-   qglVertex2f(x + scaled_width, y + scaled_height);
+	qglVertex2f(x + scaled_width, y + scaled_height);
 	qglTexCoord2f (gl->sl, gl->th);
-   qglVertex2f(x, y + scaled_height);
+	qglVertex2f(x, y + scaled_height);
 	qglEnd ();
 }
 

win32/vid_dll.c
@@ -41,7 +41,7 @@ cvar_t		*vid_ypos;			// Y coordinate of window position
 cvar_t		*vid_fullscreen;
 cvar_t		*vid_width;
 cvar_t		*vid_height;
-cvar_t      *vid_hudscale;
+cvar_t		*vid_hudscale;
 cvar_t      *vid_resolution_list; // List of resolutions, populated by DLL.
 
 // Global variables used internally by this module
@@ -420,7 +420,7 @@ void VID_Init (void)
 	win_noalttab = Cvar_Get( "win_noalttab", "0", CVAR_ARCHIVE );
 	vid_width = Cvar_Get("vid_width", "1024", CVAR_ARCHIVE);
 	vid_height = Cvar_Get("vid_height", "768", CVAR_ARCHIVE);
-   vid_hudscale = Cvar_Get("vid_hudscale", "1", CVAR_ARCHIVE);
+	vid_hudscale = Cvar_Get("vid_hudscale", "1", CVAR_ARCHIVE);
    vid_resolution_list = Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
 
 	/* Add some console commands that we want to handle */