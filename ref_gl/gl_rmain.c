bstreiff/quake2

commit 03630be2073d65a24ffdb7d613d200a58bc596de

Query resolutions from system for menu.
ref_gl now populates a cvar with a list of resolutions queried from the system, and the menu system uses that to populate the option in the video menu. (Bouncing off a cvar is kind of dopey but to do otherwise would break the DLL interface... not that we can't break the DLL interface, but I'd rather it stay vanilla for now.)

While preexisting settings for gl_mode will continue to be respected, making changes through the menu system will set gl_mode to -1 in favor of the vid_width/vid_height cvars.

ref_gl/gl_rmain.c
@@ -138,6 +138,7 @@ cvar_t	*vid_gamma;
 cvar_t	*vid_ref;
 cvar_t	*vid_width;
 cvar_t	*vid_height;
+cvar_t   *vid_resolution_list;
 
 /*
 =================
@@ -1054,6 +1055,7 @@ void R_Register( void )
 	vid_ref = ri.Cvar_Get( "vid_ref", "gl", CVAR_ARCHIVE );
 	vid_width = ri.Cvar_Get("vid_width", "1024", CVAR_ARCHIVE);
 	vid_height = ri.Cvar_Get("vid_height", "768", CVAR_ARCHIVE);
+   vid_resolution_list = ri.Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
 
 	ri.Cmd_AddCommand( "imagelist", GL_ImageList_f );
 	ri.Cmd_AddCommand( "screenshot", GL_ScreenShot_f );

win32/glw_imp.c
@@ -139,27 +139,6 @@ rserr_t GLimp_SetMode( unsigned int *pwidth, unsigned int *pheight, int mode, qb
 */
 void GLimp_Shutdown( void )
 {
-#if 0
-	if ( qwglMakeCurrent && !qwglMakeCurrent( NULL, NULL ) )
-		ri.Con_Printf( PRINT_ALL, "ref_gl::R_Shutdown() - wglMakeCurrent failed\n");
-	if ( glw_state.hGLRC )
-	{
-		if (  qwglDeleteContext && !qwglDeleteContext( glw_state.hGLRC ) )
-			ri.Con_Printf( PRINT_ALL, "ref_gl::R_Shutdown() - wglDeleteContext failed\n");
-		glw_state.hGLRC = NULL;
-	}
-	if (glw_state.hDC)
-	{
-		if ( !ReleaseDC( glw_state.hWnd, glw_state.hDC ) )
-			ri.Con_Printf( PRINT_ALL, "ref_gl::R_Shutdown() - ReleaseDC failed\n" );
-		glw_state.hDC   = NULL;
-	}
-	if (glw_state.hWnd)
-	{
-		DestroyWindow (	glw_state.hWnd );
-		glw_state.hWnd = NULL;
-	}
-#endif
 	if (glw_state.glContext)
 	{
 		SDL_GL_DeleteContext(glw_state.glContext);
@@ -177,17 +156,124 @@ void GLimp_Shutdown( void )
 		fclose( glw_state.log_fp );
 		glw_state.log_fp = 0;
 	}
-#if 0
-	UnregisterClass (WINDOW_CLASS_NAME, glw_state.hInstance);
-
-	if ( gl_state.fullscreen )
-	{
-		ChangeDisplaySettings( 0, 0 );
-		gl_state.fullscreen = false;
-	}
-#endif
 }
 
+static int VidDefCompare(const void* a, const void* b)
+{
+   const viddef_t* aa = (const viddef_t*)a;
+   const viddef_t* bb = (const viddef_t*)b;
+
+   // Sort by width first...
+   if (aa->width != bb->width)
+   {
+      return (aa->width - bb->width);
+   }
+   else
+   {
+      // If width matches, sort by height.
+      return (aa->height - bb->height);
+   }
+}
+
+void GLimp_GetResolutions(void)
+{
+   int display_count = 0;
+   int max_modes = 0;
+   SDL_DisplayMode mode_settings = {SDL_PIXELFORMAT_UNKNOWN, 0, 0, 0, 0};
+   viddef_t* resolutions = NULL;
+   size_t resolutions_count = 0;
+   char* resolution_string = NULL;
+   size_t resolution_string_len = 0;
+
+   if ((display_count = SDL_GetNumVideoDisplays()) < 1)
+      return;
+
+   // First try and figure out an upper bound on number of modes.
+   for (int display = 0; display < display_count; ++display)
+   {
+      int mode_count = 0;
+      if ((mode_count = SDL_GetNumDisplayModes(display)) < 1)
+         continue;
+      max_modes += mode_count;
+   }
+
+   resolutions = (viddef_t*)calloc(max_modes, sizeof(viddef_t));
+   if (!resolutions)
+   {
+      ri.Sys_Error(ERR_FATAL, "GetResolutions: alloc failure");
+      goto fail;
+   }
+
+   for (int display = 0; display < display_count; ++display)
+   {
+      int mode_count = 0;
+      if ((mode_count = SDL_GetNumDisplayModes(display)) < 1)
+         continue;
+
+      for (int mode = 0; mode < mode_count; ++mode)
+      {
+         viddef_t new_res;
+         qboolean found_res = false;
+         if ((SDL_GetDisplayMode(display, mode, &mode_settings)) != 0)
+            continue;
+
+         // Is this a new resolution?
+         new_res.width = mode_settings.w;
+         new_res.height = mode_settings.h;
+
+         for (int res = 0; res < resolutions_count; ++res)
+         {
+            if (VidDefCompare(&new_res, &resolutions[res]) == 0)
+            {
+               found_res = true;
+               break;
+            }
+         }
+
+         if (!found_res)
+         {
+            resolutions[resolutions_count].width = new_res.width;
+            resolutions[resolutions_count].height = new_res.height;
+            ++resolutions_count;
+         }
+      }
+      max_modes += mode_count;
+   }
+
+   // Sort the resolutions.
+   qsort(resolutions, resolutions_count, sizeof(viddef_t), VidDefCompare);
+
+   // Now turn them into a string. Conservatively assume ten characters
+   // per mode... should be enough for "XXXXXxXXXXX "
+   resolution_string_len = 10 * resolutions_count;
+   resolution_string = (char*)calloc(resolution_string_len, sizeof(char));
+   if (!resolution_string)
+   {
+      ri.Sys_Error(ERR_FATAL, "GetResolutions: alloc failure");
+      goto fail;
+   }
+
+   for (int res = 0; res < resolutions_count; ++res)
+   {
+      char tmp[20] = {0};
+      sprintf(tmp, "%s%dx%d",
+         (res == 0 ? "" : " "),
+         resolutions[res].width,
+         resolutions[res].height);
+      strcat(resolution_string, tmp);
+   }
+
+   // Without breaking the DLL interface, the only way to pass the resolution list
+   // back to the main engine is via a cvar...
+   ri.Cvar_Set("vid_resolution_list", resolution_string);
+
+fail:
+   if (resolution_string)
+      free(resolution_string);
+   if (resolutions)
+      free(resolutions);
+   return;
+}
 
 /*
 ** GLimp_Init
@@ -198,218 +284,25 @@ void GLimp_Shutdown( void )
 */
 qboolean GLimp_Init( void *hinstance, void *wndproc )
 {
-#if 0
-#define OSR2_BUILD_NUMBER 1111
-
-	OSVERSIONINFO	vinfo;
-
-	vinfo.dwOSVersionInfoSize = sizeof(vinfo);
-
-	glw_state.allowdisplaydepthchange = false;
-
-	if ( GetVersionEx( &vinfo) )
-	{
-		if ( vinfo.dwMajorVersion > 4 )
-		{
-			glw_state.allowdisplaydepthchange = true;
-		}
-		else if ( vinfo.dwMajorVersion == 4 )
-		{
-			if ( vinfo.dwPlatformId == VER_PLATFORM_WIN32_NT )
-			{
-				glw_state.allowdisplaydepthchange = true;
-			}
-			else if ( vinfo.dwPlatformId == VER_PLATFORM_WIN32_WINDOWS )
-			{
-				if ( LOWORD( vinfo.dwBuildNumber ) >= OSR2_BUILD_NUMBER )
-				{
-					glw_state.allowdisplaydepthchange = true;
-				}
-			}
-		}
-	}
-	else
-	{
-		ri.Con_Printf( PRINT_ALL, "GLimp_Init() - GetVersionEx failed\n" );
-		return false;
-	}
-
-	glw_state.hInstance = ( HINSTANCE ) hinstance;
-	glw_state.wndproc = wndproc;
-
-#endif
-
 	if (!(SDL_WasInit(SDL_INIT_VIDEO)))
 	{
 		SDL_InitSubSystem(SDL_INIT_VIDEO);
 	}
 
-	return true;
+   GLimp_GetResolutions();
+   
+   return true;
 }
 
 qboolean GLimp_InitGL (void)
 {
-#if 0
-    PIXELFORMATDESCRIPTOR pfd = 
-	{
-		sizeof(PIXELFORMATDESCRIPTOR),	// size of this pfd
-		1,								// version number
-		PFD_DRAW_TO_WINDOW |			// support window
-		PFD_SUPPORT_OPENGL |			// support OpenGL
-		PFD_DOUBLEBUFFER,				// double buffered
-		PFD_TYPE_RGBA,					// RGBA type
-		24,								// 24-bit color depth
-		0, 0, 0, 0, 0, 0,				// color bits ignored
-		0,								// no alpha buffer
-		0,								// shift bit ignored
-		0,								// no accumulation buffer
-		0, 0, 0, 0, 					// accum bits ignored
-		32,								// 32-bit z-buffer	
-		0,								// no stencil buffer
-		0,								// no auxiliary buffer
-		PFD_MAIN_PLANE,					// main layer
-		0,								// reserved
-		0, 0, 0							// layer masks ignored
-    };
-    int  pixelformat;
-
-	cvar_t *stereo;
-	
-	stereo = ri.Cvar_Get( "cl_stereo", "0", 0 );
-
-	/*
-	** set PFD_STEREO if necessary
-	*/
-	if ( stereo->value != 0 )
-	{
-		ri.Con_Printf( PRINT_ALL, "...attempting to use stereo\n" );
-		pfd.dwFlags |= PFD_STEREO;
-		gl_state.stereo_enabled = true;
-	}
-	else
-	{
-		gl_state.stereo_enabled = false;
-	}
-
-	/*
-	** figure out if we're running on a minidriver or not
-	*/
-	if ( strstr( gl_driver->string, "opengl32" ) != 0 )
-		glw_state.minidriver = false;
-	else
-		glw_state.minidriver = true;
-
-	/*
-	** Get a DC for the specified window
-	*/
-	if ( glw_state.hDC != NULL )
-		ri.Con_Printf( PRINT_ALL, "GLimp_Init() - non-NULL DC exists\n" );
-
-    if ( ( glw_state.hDC = GetDC( glw_state.hWnd ) ) == NULL )
-	{
-		ri.Con_Printf( PRINT_ALL, "GLimp_Init() - GetDC failed\n" );
-		return false;
-	}
-
-	if ( glw_state.minidriver )
-	{
-		if ( (pixelformat = qwglChoosePixelFormat( glw_state.hDC, &pfd)) == 0 )
-		{
-			ri.Con_Printf (PRINT_ALL, "GLimp_Init() - qwglChoosePixelFormat failed\n");
-			return false;
-		}
-		if ( qwglSetPixelFormat( glw_state.hDC, pixelformat, &pfd) == FALSE )
-		{
-			ri.Con_Printf (PRINT_ALL, "GLimp_Init() - qwglSetPixelFormat failed\n");
-			return false;
-		}
-		qwglDescribePixelFormat( glw_state.hDC, pixelformat, sizeof( pfd ), &pfd );
-	}
-	else
-	{
-		if ( ( pixelformat = ChoosePixelFormat( glw_state.hDC, &pfd)) == 0 )
-		{
-			ri.Con_Printf (PRINT_ALL, "GLimp_Init() - ChoosePixelFormat failed\n");
-			return false;
-		}
-		if ( SetPixelFormat( glw_state.hDC, pixelformat, &pfd) == FALSE )
-		{
-			ri.Con_Printf (PRINT_ALL, "GLimp_Init() - SetPixelFormat failed\n");
-			return false;
-		}
-		DescribePixelFormat( glw_state.hDC, pixelformat, sizeof( pfd ), &pfd );
-
-		if ( !( pfd.dwFlags & PFD_GENERIC_ACCELERATED ) )
-		{
-			extern cvar_t *gl_allow_software;
-
-			if ( gl_allow_software->value )
-				glw_state.mcd_accelerated = true;
-			else
-				glw_state.mcd_accelerated = false;
-		}
-		else
-		{
-			glw_state.mcd_accelerated = true;
-		}
-	}
-
-	/*
-	** report if stereo is desired but unavailable
-	*/
-	if ( !( pfd.dwFlags & PFD_STEREO ) && ( stereo->value != 0 ) ) 
-	{
-		ri.Con_Printf( PRINT_ALL, "...failed to select stereo pixel format\n" );
-		ri.Cvar_SetValue( "cl_stereo", 0 );
-		gl_state.stereo_enabled = false;
-	}
-
-	/*
-	** startup the OpenGL subsystem by creating a context and making
-	** it current
-	*/
-	if ( ( glw_state.hGLRC = qwglCreateContext( glw_state.hDC ) ) == 0 )
-	{
-		ri.Con_Printf (PRINT_ALL, "GLimp_Init() - qwglCreateContext failed\n");
-
-		goto fail;
-	}
-
-    if ( !qwglMakeCurrent( glw_state.hDC, glw_state.hGLRC ) )
-	{
-		ri.Con_Printf (PRINT_ALL, "GLimp_Init() - qwglMakeCurrent failed\n");
-
-		goto fail;
-	}
-#endif
 	if ( !VerifyDriver() )
 	{
 		ri.Con_Printf( PRINT_ALL, "GLimp_Init() - no hardware acceleration detected\n" );
-		goto fail;
+		return false;
 	}
-
-	/*
-	** print out PFD specifics 
-	*/
-	//ri.Con_Printf( PRINT_ALL, "GL PFD: color(%d-bits) Z(%d-bit)\n", ( int ) pfd.cColorBits, ( int ) pfd.cDepthBits );
 
 	return true;
-
-fail:
-#if 0
-	if ( glw_state.hGLRC )
-	{
-		qwglDeleteContext( glw_state.hGLRC );
-		glw_state.hGLRC = NULL;
-	}
-
-	if ( glw_state.hDC )
-	{
-		ReleaseDC( glw_state.hWnd, glw_state.hDC );
-		glw_state.hDC = NULL;
-	}
-#endif
-	return false;
 }
 
 /*

win32/vid_dll.c
@@ -42,6 +42,7 @@ cvar_t		*vid_fullscreen;
 cvar_t		*vid_width;
 cvar_t		*vid_height;
 cvar_t      *vid_hudscale;
+cvar_t      *vid_resolution_list; // List of resolutions, populated by DLL.
 
 // Global variables used internally by this module
 viddef_t	viddef;				// global video state; used by other modules
@@ -420,6 +421,7 @@ void VID_Init (void)
 	vid_width = Cvar_Get("vid_width", "1024", CVAR_ARCHIVE);
 	vid_height = Cvar_Get("vid_height", "768", CVAR_ARCHIVE);
    vid_hudscale = Cvar_Get("vid_hudscale", "1", CVAR_ARCHIVE);
+   vid_resolution_list = Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
 
 	/* Add some console commands that we want to handle */
 	Cmd_AddCommand ("vid_restart", VID_Restart_f);

commit 6a8273727da78a66dbbf9fcc78999238d9b08a42

Fix spacing issues introduced by 03630be.
Editor was replacing tabs with spaces. :(

ref_gl/gl_rmain.c
@@ -138,7 +138,7 @@ cvar_t	*vid_gamma;
 cvar_t	*vid_ref;
 cvar_t	*vid_width;
 cvar_t	*vid_height;
-cvar_t   *vid_resolution_list;
+cvar_t	*vid_resolution_list;
 
 /*
 =================
@@ -1055,7 +1055,7 @@ void R_Register( void )
 	vid_ref = ri.Cvar_Get( "vid_ref", "gl", CVAR_ARCHIVE );
 	vid_width = ri.Cvar_Get("vid_width", "1024", CVAR_ARCHIVE);
 	vid_height = ri.Cvar_Get("vid_height", "768", CVAR_ARCHIVE);
-   vid_resolution_list = ri.Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
+	vid_resolution_list = ri.Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
 
 	ri.Cmd_AddCommand( "imagelist", GL_ImageList_f );
 	ri.Cmd_AddCommand( "screenshot", GL_ScreenShot_f );

win32/glw_imp.c
@@ -160,119 +160,119 @@ void GLimp_Shutdown( void )
 
 static int VidDefCompare(const void* a, const void* b)
 {
-   const viddef_t* aa = (const viddef_t*)a;
-   const viddef_t* bb = (const viddef_t*)b;
+	const viddef_t* aa = (const viddef_t*)a;
+	const viddef_t* bb = (const viddef_t*)b;
 
-   // Sort by width first...
-   if (aa->width != bb->width)
-   {
-      return (aa->width - bb->width);
-   }
-   else
-   {
-      // If width matches, sort by height.
-      return (aa->height - bb->height);
-   }
+	// Sort by width first...
+	if (aa->width != bb->width)
+	{
+		return (aa->width - bb->width);
+	}
+	else
+	{
+		// If width matches, sort by height.
+		return (aa->height - bb->height);
+	}
 }
 
 void GLimp_GetResolutions(void)
 {
-   int display_count = 0;
-   int max_modes = 0;
-   SDL_DisplayMode mode_settings = {SDL_PIXELFORMAT_UNKNOWN, 0, 0, 0, 0};
-   viddef_t* resolutions = NULL;
-   size_t resolutions_count = 0;
-   char* resolution_string = NULL;
-   size_t resolution_string_len = 0;
+	int display_count = 0;
+	int max_modes = 0;
+	SDL_DisplayMode mode_settings = {SDL_PIXELFORMAT_UNKNOWN, 0, 0, 0, 0};
+	viddef_t* resolutions = NULL;
+	size_t resolutions_count = 0;
+	char* resolution_string = NULL;
+	size_t resolution_string_len = 0;
 
-   if ((display_count = SDL_GetNumVideoDisplays()) < 1)
-      return;
+	if ((display_count = SDL_GetNumVideoDisplays()) < 1)
+		return;
 
-   // First try and figure out an upper bound on number of modes.
-   for (int display = 0; display < display_count; ++display)
-   {
-      int mode_count = 0;
-      if ((mode_count = SDL_GetNumDisplayModes(display)) < 1)
-         continue;
-      max_modes += mode_count;
-   }
+	// First try and figure out an upper bound on number of modes.
+	for (int display = 0; display < display_count; ++display)
+	{
+		int mode_count = 0;
+		if ((mode_count = SDL_GetNumDisplayModes(display)) < 1)
+			continue;
+		max_modes += mode_count;
+	}
 
-   resolutions = (viddef_t*)calloc(max_modes, sizeof(viddef_t));
-   if (!resolutions)
-   {
-      ri.Sys_Error(ERR_FATAL, "GetResolutions: alloc failure");
-      goto fail;
-   }
+	resolutions = (viddef_t*)calloc(max_modes, sizeof(viddef_t));
+	if (!resolutions)
+	{
+		ri.Sys_Error(ERR_FATAL, "GetResolutions: alloc failure");
+		goto fail;
+	}
 
-   for (int display = 0; display < display_count; ++display)
-   {
-      int mode_count = 0;
-      if ((mode_count = SDL_GetNumDisplayModes(display)) < 1)
-         continue;
+	for (int display = 0; display < display_count; ++display)
+	{
+		int mode_count = 0;
+		if ((mode_count = SDL_GetNumDisplayModes(display)) < 1)
+			continue;
 
-      for (int mode = 0; mode < mode_count; ++mode)
-      {
-         viddef_t new_res;
-         qboolean found_res = false;
-         if ((SDL_GetDisplayMode(display, mode, &mode_settings)) != 0)
-            continue;
+		for (int mode = 0; mode < mode_count; ++mode)
+		{
+			viddef_t new_res;
+			qboolean found_res = false;
+			if ((SDL_GetDisplayMode(display, mode, &mode_settings)) != 0)
+			continue;
 
-         // Is this a new resolution?
-         new_res.width = mode_settings.w;
-         new_res.height = mode_settings.h;
+			// Is this a new resolution?
+			new_res.width = mode_settings.w;
+			new_res.height = mode_settings.h;
 
-         for (int res = 0; res < resolutions_count; ++res)
-         {
-            if (VidDefCompare(&new_res, &resolutions[res]) == 0)
-            {
-               found_res = true;
-               break;
-            }
-         }
+			for (int res = 0; res < resolutions_count; ++res)
+			{
+				if (VidDefCompare(&new_res, &resolutions[res]) == 0)
+				{
+					found_res = true;
+					break;
+				}
+			}
 
-         if (!found_res)
-         {
-            resolutions[resolutions_count].width = new_res.width;
-            resolutions[resolutions_count].height = new_res.height;
-            ++resolutions_count;
-         }
-      }
-      max_modes += mode_count;
-   }
+			if (!found_res)
+			{
+			resolutions[resolutions_count].width = new_res.width;
+			resolutions[resolutions_count].height = new_res.height;
+			++resolutions_count;
+			}
+		}
+		max_modes += mode_count;
+	}
 
-   // Sort the resolutions.
-   qsort(resolutions, resolutions_count, sizeof(viddef_t), VidDefCompare);
+	// Sort the resolutions.
+	qsort(resolutions, resolutions_count, sizeof(viddef_t), VidDefCompare);
 
-   // Now turn them into a string. Conservatively assume ten characters
-   // per mode... should be enough for "XXXXXxXXXXX "
-   resolution_string_len = 10 * resolutions_count;
-   resolution_string = (char*)calloc(resolution_string_len, sizeof(char));
-   if (!resolution_string)
-   {
-      ri.Sys_Error(ERR_FATAL, "GetResolutions: alloc failure");
-      goto fail;
-   }
+	// Now turn them into a string. Conservatively assume ten characters
+	// per mode... should be enough for "XXXXXxXXXXX "
+	resolution_string_len = 10 * resolutions_count;
+	resolution_string = (char*)calloc(resolution_string_len, sizeof(char));
+	if (!resolution_string)
+	{
+		ri.Sys_Error(ERR_FATAL, "GetResolutions: alloc failure");
+		goto fail;
+	}
 
-   for (int res = 0; res < resolutions_count; ++res)
-   {
-      char tmp[20] = {0};
-      sprintf(tmp, "%s%dx%d",
-         (res == 0 ? "" : " "),
-         resolutions[res].width,
-         resolutions[res].height);
-      strcat(resolution_string, tmp);
-   }
+	for (int res = 0; res < resolutions_count; ++res)
+	{
+		char tmp[20] = {0};
+		sprintf(tmp, "%s%dx%d",
+			(res == 0 ? "" : " "),
+			resolutions[res].width,
+			resolutions[res].height);
+		strcat(resolution_string, tmp);
+	}
 
-   // Without breaking the DLL interface, the only way to pass the resolution list
-   // back to the main engine is via a cvar...
-   ri.Cvar_Set("vid_resolution_list", resolution_string);
+	// Without breaking the DLL interface, the only way to pass the resolution list
+	// back to the main engine is via a cvar...
+	ri.Cvar_Set("vid_resolution_list", resolution_string);
 
 fail:
-   if (resolution_string)
-      free(resolution_string);
-   if (resolutions)
-      free(resolutions);
-   return;
+	if (resolution_string)
+		free(resolution_string);
+	if (resolutions)
+		free(resolutions);
+	return;
 }
 
 /*
@@ -289,9 +289,9 @@ qboolean GLimp_Init( void *hinstance, void *wndproc )
 		SDL_InitSubSystem(SDL_INIT_VIDEO);
 	}
 
-   GLimp_GetResolutions();
-   
-   return true;
+	GLimp_GetResolutions();
+
+	return true;
 }
 
 qboolean GLimp_InitGL (void)

win32/vid_dll.c
@@ -42,7 +42,7 @@ cvar_t		*vid_fullscreen;
 cvar_t		*vid_width;
 cvar_t		*vid_height;
 cvar_t		*vid_hudscale;
-cvar_t      *vid_resolution_list; // List of resolutions, populated by DLL.
+cvar_t		*vid_resolution_list; // List of resolutions, populated by DLL.
 
 // Global variables used internally by this module
 viddef_t	viddef;				// global video state; used by other modules
@@ -196,9 +196,9 @@ void VID_Front_f( void )
 */
 typedef struct vidmode_s
 {
-	const char *description;
-	int         width, height;
-	int         mode;
+	const char	*description;
+	int		width, height;
+	int		mode;
 } vidmode_t;
 
 vidmode_t vid_modes[] =
@@ -421,7 +421,7 @@ void VID_Init (void)
 	vid_width = Cvar_Get("vid_width", "1024", CVAR_ARCHIVE);
 	vid_height = Cvar_Get("vid_height", "768", CVAR_ARCHIVE);
 	vid_hudscale = Cvar_Get("vid_hudscale", "1", CVAR_ARCHIVE);
-   vid_resolution_list = Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
+	vid_resolution_list = Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
 
 	/* Add some console commands that we want to handle */
 	Cmd_AddCommand ("vid_restart", VID_Restart_f);

win32/vid_menu.c
@@ -113,9 +113,9 @@ static void ApplyChanges( void *unused )
 	Cvar_SetValue( "vid_fullscreen", s_fs_box[s_current_menu_index].curvalue );
 	Cvar_SetValue( "gl_ext_palettedtexture", s_paletted_texture_box.curvalue );
 	Cvar_SetValue( "gl_finish", s_finish_box.curvalue );
-   Cvar_SetValue( "gl_mode", -1);
-   Cvar_SetValue( "vid_height", s_possible_resolutions_list[s_mode_list[OPENGL_MENU].curvalue].height);
-   Cvar_SetValue( "vid_width", s_possible_resolutions_list[s_mode_list[OPENGL_MENU].curvalue].width);
+	Cvar_SetValue( "gl_mode", -1);
+	Cvar_SetValue( "vid_height", s_possible_resolutions_list[s_mode_list[OPENGL_MENU].curvalue].height);
+	Cvar_SetValue( "vid_width", s_possible_resolutions_list[s_mode_list[OPENGL_MENU].curvalue].width);
 
 	switch ( s_ref_list[s_current_menu_index].curvalue )
 	{
@@ -167,14 +167,14 @@ static void ApplyChanges( void *unused )
 			vid_ref->modified = true;
 	}
 
-   VID_MenuDestroy();
+	VID_MenuDestroy();
 	M_ForceMenuOff();
 }
 
 void M_PopMenu(void);
 static void CancelChanges( void *unused )
 {
-   VID_MenuDestroy();
+	VID_MenuDestroy();
 	M_PopMenu();
 }
 
@@ -182,115 +182,115 @@ static void VID_DestroyResolutionList();
 
 static void VID_CreateResolutionList()
 {
-   cvar_t* vid_resolution_list = Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
-   char* itr = vid_resolution_list->string;
-   int res_count = 0;
+	cvar_t* vid_resolution_list = Cvar_Get("vid_resolution_list", "640x480 800x600 1024x768 1152x864 1280x960 1600x1200", 0);
+	char* itr = vid_resolution_list->string;
+	int res_count = 0;
 
-   // Make sure the list is cleared out before we start...
-   VID_DestroyResolutionList();
+	// Make sure the list is cleared out before we start...
+	VID_DestroyResolutionList();
 
-   // Count number of spaces in the string. This is number of resolutions, minus one.
-   while (*itr != '\0')
-   {
-      if (*itr == ' ')
-         res_count++;
-      ++itr;
-   }
-   res_count++;
+	// Count number of spaces in the string. This is number of resolutions, minus one.
+	while (*itr != '\0')
+	{
+		if (*itr == ' ')
+			res_count++;
+		++itr;
+	}
+	res_count++;
 
-   s_possible_resolutions_list_count = res_count;
-   s_possible_resolutions_list = (viddef_t*)calloc(res_count, sizeof(viddef_t));
-   if (!s_possible_resolutions_list)
-   {
-      Sys_Error(ERR_FATAL, "VID_CreateResolutionList: alloc failure");
-      return;
-   }
+	s_possible_resolutions_list_count = res_count;
+	s_possible_resolutions_list = (viddef_t*)calloc(res_count, sizeof(viddef_t));
+	if (!s_possible_resolutions_list)
+	{
+		Sys_Error(ERR_FATAL, "VID_CreateResolutionList: alloc failure");
+		return;
+	}
 
-   // Move itr back to start of string
-   itr = vid_resolution_list->string;
+	// Move itr back to start of string
+	itr = vid_resolution_list->string;
 
-   for (int res = 0; res < res_count; ++res)
-   {
-      long int width, height;
+	for (int res = 0; res < res_count; ++res)
+	{
+		long int width, height;
 
-      width = strtol(itr, &itr, 10);
-      ++itr; // skip over "x"
-      height = strtol(itr, &itr, 10);
+		width = strtol(itr, &itr, 10);
+		++itr; // skip over "x"
+		height = strtol(itr, &itr, 10);
 
-      s_possible_resolutions_list[res].height = (int)height;
-      s_possible_resolutions_list[res].width = (int)width;
-   }
+		s_possible_resolutions_list[res].height = (int)height;
+		s_possible_resolutions_list[res].width = (int)width;
+	}
 
-   // Now, convert the resolution list into strings. Need +1 for terminator.
-   s_possible_resolutions_str_list = (char**)calloc(res_count+1, sizeof(char*));
-   if (!s_possible_resolutions_str_list)
-   {
-      Sys_Error(ERR_FATAL, "VID_CreateResolutionList: alloc failure");
-      return;
-   }
+	// Now, convert the resolution list into strings. Need +1 for terminator.
+	s_possible_resolutions_str_list = (char**)calloc(res_count+1, sizeof(char*));
+	if (!s_possible_resolutions_str_list)
+	{
+		Sys_Error(ERR_FATAL, "VID_CreateResolutionList: alloc failure");
+		return;
+	}
 
-   for (int res = 0; res < res_count; ++res)
-   {
-      const int field_width = 14;
+	for (int res = 0; res < res_count; ++res)
+	{
+		const int field_width = 14;
 
-      s_possible_resolutions_str_list[res] = (char*)calloc(field_width, sizeof(char));
-      if (!s_possible_resolutions_str_list[res])
-      {
-         Sys_Error(ERR_FATAL, "VID_CreateResolutionList: alloc failure");
-         return;
-      }
+		s_possible_resolutions_str_list[res] = (char*)calloc(field_width, sizeof(char));
+		if (!s_possible_resolutions_str_list[res])
+		{
+			Sys_Error(ERR_FATAL, "VID_CreateResolutionList: alloc failure");
+			return;
+		}
 
-      Com_sprintf(s_possible_resolutions_str_list[res], field_width-1, "[%d %d",
-         s_possible_resolutions_list[res].width,
-         s_possible_resolutions_list[res].height);
+		Com_sprintf(s_possible_resolutions_str_list[res], field_width-1, "[%d %d",
+			s_possible_resolutions_list[res].width,
+			s_possible_resolutions_list[res].height);
 
-      // From right, add the ']' and pad with spaces.
-      s_possible_resolutions_str_list[res][field_width-1] = '\0';
-      s_possible_resolutions_str_list[res][field_width-2] = ']';
-      for (int i = field_width-3; i > 0; --i)
-      {
-         if (s_possible_resolutions_str_list[res][i] == '\0')
-            s_possible_resolutions_str_list[res][i] = ' ';
-      }
-   }
+		// From right, add the ']' and pad with spaces.
+		s_possible_resolutions_str_list[res][field_width-1] = '\0';
+		s_possible_resolutions_str_list[res][field_width-2] = ']';
+		for (int i = field_width-3; i > 0; --i)
+		{
+			if (s_possible_resolutions_str_list[res][i] == '\0')
+			s_possible_resolutions_str_list[res][i] = ' ';
+		}
+	}
 }
 
 static int VID_FindIndexForResolution(int width, int height)
 {
-   // First, look for an exact match.
-   for (int res = 0; res < s_possible_resolutions_list_count; ++res)
-   {
-      if (s_possible_resolutions_list[res].width == width &&
-          s_possible_resolutions_list[res].height == height)
-      {
-         return res;
-      }
-   }
+	// First, look for an exact match.
+	for (int res = 0; res < s_possible_resolutions_list_count; ++res)
+	{
+		if (s_possible_resolutions_list[res].width == width &&
+		    s_possible_resolutions_list[res].height == height)
+		{
+			return res;
+		}
+	}
 
-   // No exact match, just pick the first one.
-   return 0;
+	// No exact match, just pick the first one.
+	return 0;
 }
 
 static void VID_DestroyResolutionList()
 {
-   if (s_possible_resolutions_list)
-   {
-      free(s_possible_resolutions_list);
-      s_possible_resolutions_list = NULL;
-      s_possible_resolutions_list_count = 0;
-   }
-   if (s_possible_resolutions_str_list)
-   {
-      char** itr = s_possible_resolutions_str_list;
-      while (*itr != NULL)
-      {
-         free(*itr);
-         *itr = NULL;
-         ++itr;
-      }
-      free(s_possible_resolutions_str_list);
-      s_possible_resolutions_str_list = NULL;
-   }
+	if (s_possible_resolutions_list)
+	{
+		free(s_possible_resolutions_list);
+		s_possible_resolutions_list = NULL;
+		s_possible_resolutions_list_count = 0;
+	}
+	if (s_possible_resolutions_str_list)
+	{
+		char** itr = s_possible_resolutions_str_list;
+		while (*itr != NULL)
+		{
+			free(*itr);
+			*itr = NULL;
+			++itr;
+		}
+		free(s_possible_resolutions_str_list);
+		s_possible_resolutions_str_list = NULL;
+	}
 }
 
 /*
@@ -298,7 +298,7 @@ static void VID_DestroyResolutionList()
 */
 void VID_MenuInit( void )
 {
-   const float scale = vid_hudscale->value;
+	const float scale = vid_hudscale->value;
 
 	static const char *refs[] =
 	{
@@ -315,12 +315,12 @@ void VID_MenuInit( void )
 		0
 	};
 	int i;
-   int width;
-   int height;
+	int width;
+	int height;
 
-   VID_CreateResolutionList();
-   
-   if ( !gl_driver )
+	VID_CreateResolutionList();
+
+	if ( !gl_driver )
 		gl_driver = Cvar_Get( "gl_driver", "opengl32", 0 );
 	if ( !gl_picmip )
 		gl_picmip = Cvar_Get( "gl_picmip", "0", 0 );
@@ -331,10 +331,10 @@ void VID_MenuInit( void )
 	if ( !gl_finish )
 		gl_finish = Cvar_Get( "gl_finish", "0", CVAR_ARCHIVE );
 
-   // Get current width and height.
-   VID_GetModeInfo(&width, &height, gl_mode->value);
-   s_mode_list[OPENGL_MENU].curvalue = VID_FindIndexForResolution(width, height);
-   
+	// Get current width and height.
+	VID_GetModeInfo(&width, &height, gl_mode->value);
+	s_mode_list[OPENGL_MENU].curvalue = VID_FindIndexForResolution(width, height);
+
 	if ( strcmp( vid_ref->string, "gl" ) == 0 )
 	{
 		s_current_menu_index = OPENGL_MENU;
@@ -435,7 +435,7 @@ void VID_MenuInit( void )
 
 static void VID_MenuDestroy()
 {
-   VID_DestroyResolutionList();
+	VID_DestroyResolutionList();
 }
 
 /*